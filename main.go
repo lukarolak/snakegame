5bc35097077da28df4ca84590936dc3de357ee4a
zohraBoukhatem <zohra.boukhatem1@gmail.com>
2023-07-27 20:24:45 +0200

    added game.go

--- main.go
+++ main.go
@@ -6,19 +6,6 @@ import (
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
-type Game struct{}
-
-func (g *Game) Update() error {
-	return nil
-}
-
-func (g *Game) Draw(screen *ebiten.Image) {
-}
-
-func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-	return 320, 240
-}
-
 func main() {
 	ebiten.SetWindowSize(700, 500)
 	ebiten.SetWindowTitle("snakegame")
